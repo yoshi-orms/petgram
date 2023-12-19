2588ba861e90fcb5c7cd29d0c2457a9641936ecc
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-12-19 23:03:55 +0900

    昇順に表示されるようにした

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,7 +2,7 @@ class PetsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @pets = Pet.all
+    @pets = Pet.all.order(id: "DESC")
   end
 
   def new
