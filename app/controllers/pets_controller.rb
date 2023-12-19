a6bc140414680c477a4c18d902039694f37635b4
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-12-19 13:17:19 +0900

    投稿できるようにcreateを実装

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,10 +7,21 @@ class PetsController < ApplicationController
     @pet = Pet.new
   end
 
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user_id = current_user.id
+    @pet.save
+    redirect_to pet_path(@pet)
+  end
+
   def show
   end
 
   def edit
   end
 
+  private
+  def pet_params
+    params.require(:pet).permit(:title, :body, :image)
+  end
 end
