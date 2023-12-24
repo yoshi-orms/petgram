a02673bb6a064cd8698d395a4a21b0f95d4cc721
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-12-25 02:09:16 +0900

    migrateの編集

--- db/migrate/20231219033157_create_pets.rb
+++ db/migrate/20231219033157_create_pets.rb
@@ -1,7 +1,7 @@
 class CreatePets < ActiveRecord::Migration[7.0]
   def change
     create_table :pets do |t|
-      t.integer :user_id, null: false
+      t.references :user, null: false, foreign_key: true
       t.string :title, null: false
       t.text :body, null: false
       t.string :image_id, null: false
