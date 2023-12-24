e2d98e654457e93993c9dc25a9af0a5112f02e9b
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-12-25 02:21:40 +0900

    モデルへ条件文の記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,17 @@ class User < ApplicationRecord
   attachment :profile_image
 
   has_many :pets, dependent: :destroy
-  
+
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
   has_many :followings, through: :active_relationships, source: :follower
 
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+    follower =  passive_relationships.find_by(following_id: user.id)
+    return follower.present?
+  end
 
   validates :username, presence: true
 end
