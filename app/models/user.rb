1e62e5ff5f29f8b9c83b637700a08ccb77894706
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-12-25 02:03:34 +0900

    Userモデルの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,12 @@ class User < ApplicationRecord
   attachment :profile_image
 
   has_many :pets, dependent: :destroy
+  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
   validates :username, presence: true
 end
