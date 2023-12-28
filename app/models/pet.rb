3e6f9f4ffbeaa6d41ec2c5f3ba32eed40cae1688
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-12-28 14:13:36 +0900

    該当ツイートをいいね済かどうか判断する記述

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,9 @@ class Pet < ApplicationRecord
   attachment :image
   belongs_to :user
   has_many :likes
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 
   with_options presence: true do
     validates :title
