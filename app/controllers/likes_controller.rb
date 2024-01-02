3d8148dfc62432f6c58ac7cbd5f3b2d5997e7d10
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2024-01-02 14:44:16 +0900

    エラーが発生する為修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,13 @@
 class LikesController < ApplicationController
-  before_action :set_pet
-
   def create
     like = current_user.likes.build(pet_id: params[:pet_id])
     like.save
-    respond_to do |format|
-      format.js
-    end
+    redirect_to pets_path
   end
 
   def destroy
     like = Like.find_by(pet_id: params[:pet_id], user_id: current_user.id)
     like.destroy
-    respond_to do |format|
-      format.js
-      format.turbo_stream
-    end
-  end
-
-  def set_pet
-    @pet = Pet.find(params[:pet_id])
+    redirect_to pets_path
   end
 end
