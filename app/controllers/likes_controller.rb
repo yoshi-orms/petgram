cfd38f344d520124f09361ee424c89f16b5ae07f
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2024-01-02 14:19:51 +0900

    コントローラーの記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,23 @@
 class LikesController < ApplicationController
+  before_action :set_pet
+
   def create
     like = current_user.likes.build(pet_id: params[:pet_id])
     like.save
-    redirect_to pets_path
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
     like = Like.find_by(pet_id: params[:pet_id], user_id: current_user.id)
     like.destroy
-    redirect_to pets_path
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:pet_id])
   end
 end
